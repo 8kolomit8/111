f69565fccbc8df02ff3654d48ecbe22349284321
8kolomit8 <waldemar21882@gmail.com>
2024-10-30 08:59:47 +0300

    1132

--- qqqw.py
+++ qqqw.py
@@ -3,6 +3,6 @@ m1 = 400
 g = 9.81
 def a1(M, m1, m2):
     return -(m1 - m2) * g / (m1 + m2 + M)
-for m2 in range(10, 301, 20):
+for m2 in range(100, 301, 20):
     a = a1(M, m1, m2)
     print(f"для m2 = {m2}, ускорение a = {a:.2f}")
