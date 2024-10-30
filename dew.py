c25a68ef4208681b7534ea7cafe1bf9a6d539aa8
8kolomit8 <waldemar21882@gmail.com>
2024-10-30 09:10:04 +0300

    1132

--- dew.py
+++ dew.py
@@ -1,6 +1,7 @@
-def a(c):
-    return 1.8 * c + 32
-print ("Цельсия (°C) Фаренгейт (°F)")
-for c in range(51, 29, -15):
-    f = a(c)
-    print(f"{c} {f}")
+def c1(c2):
+    return 1.8 * c2 + 32
+print("Цельсии (°C) Фаренгейт (°F)")
+for c in range(15, 31, 2):
+    c2 = c / 2
+    f = c1(c2)
+    print(f"{c2} {f:.1f}")
