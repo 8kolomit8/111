19ad9b6c82df93bab6349c595ef4d8c219ad7b5d
8kolomit8 <waldemar21882@gmail.com>
2024-10-30 09:34:35 +0300

    1132

--- dw.py
+++ dw.py
@@ -1,4 +1,12 @@
-A = float(input("A (> 1): "))
+while True:
+    while True:
+       try: A = float(input('A = '))
+       except ValueError:
+           print('Введены некорректные данные')
+       else: break
+    if A <= 1:
+        print('Введены некоректные данные')
+    else: break
 k = 0
 sum = 0
 while sum <= A:
