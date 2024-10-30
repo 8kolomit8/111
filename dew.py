aa013606135edcdf28bee4882fb4fade7ecf84ce
8kolomit8 <waldemar21882@gmail.com>
2024-10-30 09:25:53 +0300

    1132

--- dew.py
+++ dew.py
@@ -1,7 +1,9 @@
 def c1(c2):
     return 1.8 * c2 + 32
+c2=15
 print("Цельсии (°C) Фаренгейт (°F)")
-for c in range(15, 31, 2):
-    c2 = c / 2
+while (c2<=30):
     f = c1(c2)
     print(f"{c2} {f:.1f}")
+    c2+=1.5
+
--- dw.py
+++ dw.py
@@ -1,6 +1,6 @@
 A = float(input("Введите число A (> 1): "))
 k = 0
-sum = 0.0
+sum = 0
 while sum <= A:
     k += 1
     sum += 1 / k
