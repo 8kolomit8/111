0d2c40daec273fb6baab3dc3c146b196b7021ca4
8kolomit8 <waldemar21882@gmail.com>
2024-10-30 09:40:31 +0300

    1132

--- qqqw.py
+++ qqqw.py
@@ -4,4 +4,5 @@ g = 9.81
 def a1(M, m1, m2):
     return -(m2 - m1) * g / (m1 + m2 + (m1+m2)/2)
 for m2 in range(100, 301, 20):
-    print(f"m2 = {m2}, a = {a1(M, m1, m2):.2f}")
+    print(f"m2={m2}, a={a1(M, m1, m2):.2f}")
+
